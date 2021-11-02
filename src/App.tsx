1b1d62c99c8f5e0dcf2fa45bf9d77c97feb5a111
andre9653 <andresena9653@gmail.com>
2021-11-02 01:51:55 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-use-before-define
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <div />
+    </div>
+  );
+}
+
+export default App;
