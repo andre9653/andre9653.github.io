9518c322ee24de7a1039deb073fe9013024482f6
andre9653 <andresena9653@gmail.com>
2021-11-05 00:00:16 -0300

    Adiciona elementos a aba projects

--- src/components/MainProjects/index.tsx
+++ src/components/MainProjects/index.tsx
@@ -1,10 +1,17 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
+import { Container } from "./style";
 
 export default function MainProjects() {
   return (
-    <section className="md:container md:mx-auto">
+    <Container className="md:container md:mx-auto">
       <h2 className="title-projects">Projects</h2>
-    </section>
+      <div className="container-projects">
+        <div>projeto 1</div>
+        <div>projeto 2</div>
+        <div>projeto 3</div>
+        <div>projeto 4</div>
+      </div>
+    </Container>
   );
 }
--- src/components/MainProjects/style.ts
+++ src/components/MainProjects/style.ts
@@ -1,8 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  display: flex;
-  color: white;
   grid-area: "main-projects";
+  display: grid;
+  color: white;
+  flex-direction: column;
   height: 100%;
+  grid-template-rows: 1fr 4.5fr;
+  justify-items: center;
+  align-items: center;
+
+  h2 {
+    font-weight: 600;
+    font-size: 2rem;
+  }
+
+  .container-projects {
+    width: 80%;
+    height: 90%;
+    display: flex;
+    background-color: rgba(0, 0, 0, 50%);
+    align-items: center;
+    margin-bottom: 30px;
+  }
+
+  .container-projects div {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 50%);
+  }
 `;
